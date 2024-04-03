6dcb210b3e055b81ae2aea7a75c3d4d222f9bcf6
Muhammad Oka <oka@muhammadoka.dev>
2024-04-03 15:59:40 +0700

    change <leader>x to toggle nvimtree

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -1,47 +1,47 @@
 return {
-  {
-    'nvim-tree/nvim-tree.lua',
-    version = '*',
-    lazy = false,
-    dependencies = {
-      'nvim-tree/nvim-web-devicons',
-    },
-    config = function()
-      require('nvim-tree').setup {
-        vim.keymap.set('n', '<leader>x', ':NvimTreeOpen<CR>', { desc = 'Open Explorer' }),
-      }
-    end,
-  },
-  {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      require('lualine').setup {
-        disabled_filetypes = { 'packer', 'NvimTree' },
-      }
-    end,
-  },
-  {
-    'ThePrimeagen/harpoon',
-    branch = 'harpoon2',
-    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim' },
-  },
-  {
-    'iamcco/markdown-preview.nvim',
-    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-    ft = { 'markdown' },
-    build = function()
-      vim.fn['mkdp#util#install']()
-    end,
-  },
-  -- {
-  --   'Pocco81/auto-save.nvim',
-  --   config = function()
-  --     require('auto-save').setup {
-  --       enabled = false,
-  --       debounce_delay = 1000,
-  --       vim.keymap.set('n', '<leader>as', ':ASToggle<CR>'),
-  --     }
-  --   end,
-  -- },
+	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		lazy = false,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("nvim-tree").setup({
+				vim.keymap.set("n", "<leader>x", ":NvimTreeToggle<CR>", { desc = "Toggle Explorer" }),
+			})
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup({
+				disabled_filetypes = { "packer", "NvimTree" },
+			})
+		end,
+	},
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+	},
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
+	-- {
+	--   'Pocco81/auto-save.nvim',
+	--   config = function()
+	--     require('auto-save').setup {
+	--       enabled = false,
+	--       debounce_delay = 1000,
+	--       vim.keymap.set('n', '<leader>as', ':ASToggle<CR>'),
+	--     }
+	--   end,
+	-- },
 }
