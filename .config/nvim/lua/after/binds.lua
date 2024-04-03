e3cdd31f9179269522007a1b42aad708aed4da0d
Muhammad Oka <oka@muhammadoka.dev>
2024-04-03 14:36:54 +0700

    add powerful paste nvim bind

--- .config/nvim/lua/after/binds.lua
+++ .config/nvim/lua/after/binds.lua
@@ -1,1 +1,2 @@
-vim.keymap.set('n', '<leader>lf', ':%s/\\r//g<CR>', { desc = 'Remove ^M' })
+vim.keymap.set("n", "<leader>lf", ":%s/\\r//g<CR>", { desc = "Remove ^M" })
+vim.keymap.set("x", "p", [["_dP]])
