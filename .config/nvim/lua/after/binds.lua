1b559eedaef6669dade9cad229594a41b0526d4e
Muhammad Oka <oka@muhammadoka.dev>
2024-04-04 14:09:41 +0700

    add remaps for yank and delete behavior

--- .config/nvim/lua/after/binds.lua
+++ .config/nvim/lua/after/binds.lua
@@ -4,8 +4,13 @@ vim.keymap.set("n", "<leader>lf", ":%s/\\r//g<CR>", { desc = "Remove ^M" })
 -- Best remap ever
 vim.keymap.set("x", "p", [["_dP]])
 
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({ "n", "v" }, "y", [["+y]])
+vim.keymap.set("n", "Y", [["+Y]])
+vim.keymap.set({ "n", "v" }, "d", [["_d]])
+
 -- Move line keybind
-vim.keymap.set("n", "<A-j>", ":m +1<CR>", { desc = "Move current line down" })
 vim.keymap.set("n", "<A-k>", ":m -2<CR>", { desc = "Move current line up" })
+vim.keymap.set("n", "<A-j>", ":m +1<CR>", { desc = "Move current line down" })
 vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move current line down" })
 vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move current line up" })
