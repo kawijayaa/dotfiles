838038833872529d5589e75e57c02dab713485fa
Muhammad Oka <oka@muhammadoka.dev>
2024-04-05 16:21:45 +0700

    change delete keybind to use leader

--- .config/nvim/lua/after/binds.lua
+++ .config/nvim/lua/after/binds.lua
@@ -7,7 +7,7 @@ vim.keymap.set("x", "p", [["_dP]])
 -- next greatest remap ever : asbjornHaland
 vim.keymap.set({ "n", "v" }, "y", [["+y]])
 vim.keymap.set("n", "Y", [["+Y]])
-vim.keymap.set({ "n", "v" }, "d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 -- Move line keybind
 vim.keymap.set("n", "<A-k>", ":m -2<CR>", { desc = "Move current line up" })
