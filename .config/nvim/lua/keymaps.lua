9f2fec37013b2b17795111f1e9775e77c95fe4de
Muhammad Oka <oka@muhammadoka.dev>
2024-04-07 19:27:41 +0700

    use mini.move for moving selections instead of custom keymaps

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -39,12 +39,6 @@ vim.keymap.set({ "n", "v" }, "y", [["+y]])
 vim.keymap.set("n", "Y", [["+Y]])
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
--- Move line keybind
-vim.keymap.set("n", "<A-k>", ":m -2<CR>", { desc = "Move current line up" })
-vim.keymap.set("n", "<A-j>", ":m +1<CR>", { desc = "Move current line down" })
-vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move current line down" })
-vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move current line up" })
-
 -- Insert indented when line is empty
 vim.keymap.set("n", "i", function()
 	return string.match(vim.api.nvim_get_current_line(), "%g") == nil and "cc" or "i"
