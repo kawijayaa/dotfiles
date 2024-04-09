46ab04dbd70755769862733567d9e89f4f524be1
Muhammad Oka <oka@muhammadoka.dev>
2024-04-10 00:43:29 +0700

    use global status line and use relative path for filename

--- .config/nvim/lua/kawijayaa/plugins/lualine.lua
+++ .config/nvim/lua/kawijayaa/plugins/lualine.lua
@@ -7,11 +7,12 @@ return {
 				options = {
 					section_separators = "",
 					component_separators = { left = "❭", right = "❬" },
+					globalstatus = true,
 				},
 				sections = {
 					lualine_a = { "mode" },
 					lualine_b = { "branch", "diff", "diagnostics" },
-					lualine_c = { "filename" },
+					lualine_c = { { "filename", path = 1 } },
 					lualine_x = { "encoding", "fileformat", "filetype", "location" },
 					lualine_y = {},
 					lualine_z = {},
